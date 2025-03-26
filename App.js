e9f0681ab1295187660c313a3e6efce028f653fe
Paulo Galarza <paulo-123@hotmail.es>
2025-03-26 11:15:50 -0500

    Pantalla de nueva cita y ventana modal para registrar nueva cita

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Pressable } from "react-native";
+import Formulario from "./src/components/Formulario";
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>
+        Administrador de Citas {""}
+        <Text style={styles.tituloBold}>Veterinaria ITUJ</Text>
+      </Text>
+
+      <Pressable
+        style={styles.btnNuevaCita}
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      >
+        <Text style={styles.btnTextNuevaCita}>Nueva Cita</Text>
+      </Pressable>
+      <Formulario />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  titulo: {
+    textAlign: "center",
+    fontSize: 30,
+    color: "#374151",
+    fontWeight: "600",
+  },
+  tituloBold: {
+    fontWeight: "900",
+    color: "#6028D9",
+  },
   container: {
+    backgroundColor: "#f3f4f6",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  btnNuevaCita: {
+    backgroundColor: "#6028D9",
+    padding: 15,
+    marginTop: 30,
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  btnTextNuevaCita: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "900",
+    textAlign: "center",
   },
 });
+
+{
+  /* <button onclick="descargarCitas()">Descargar citas</button>
+
+<Button onClick={descargarCitas()}>Descargar citas</Button> */
+}
